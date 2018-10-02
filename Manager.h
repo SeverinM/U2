f6a28677a9743494bf7b4042f77d252cf6ecd02d
SeverinM <severinmichaut@gmail.com>
2018-10-02 11:36:54 +0200

    Main Loop un peu mieux

--- Manager.h
+++ Manager.h
@@ -1,16 +1,18 @@
 #ifndef MANAGER_H
 #define MANAGER_H
+#define SIZEX 100
+#define SIZEY 100
 #include <windows.h>
 
 class Manager
 {
     public:
         Manager();
-        virtual ~Manager();
-        void MainLoop();
-
+        void MainLoop(float time);
+        void stopLoop();
+        bool isStop();
     private:
-        CHAR_INFO buffer[700][700];
+        CHAR_INFO buffer[SIZEX][SIZEY];
         bool stop;
         COORD bufferSize;
         COORD bufferCoord;
