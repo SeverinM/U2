0d84873df9bc0044bd85a752cb057072cf265060
Unknown <pifi77@hotmail.fr>
2018-10-02 11:45:01 +0200

    Move set
    Move set in another script (moveMain)

---
+++ moveMain.h
@@ -0,0 +1,16 @@
+#ifndef MOVEMAIN_H
+#define MOVEMAIN_H
+
+
+class moveMain
+{
+    public:
+        moveMain();
+        virtual ~moveMain();
+
+    protected:
+
+    private:
+};
+
+#endif // MOVEMAIN_H
