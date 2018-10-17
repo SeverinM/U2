79c803b48595e8af599395f26d149f75165bfda4
SeverinM <severinmichaut@gmail.com>
2018-10-17 20:39:00 +0200

    supression de moveMain + nouvelle lib pour lire JSON

--- src/Texte.cpp
+++ src/Texte.cpp
@@ -1,8 +1,18 @@
 #include "Texte.h"
+#include <jansson.h>
 
 Texte::Texte(int startPosX, int startPosY) : Positionable(startPosX, startPosY)
 {
-    //ctor
+
+    // A supprimer , servait juste a voir si la lib a bien été linké
+    json_t *arr, *integ;
+
+    arr = json_array();
+    integ = json_integer(42);
+
+    json_array_append(arr, integ);
+    json_decref(integ);
+
 }
 
 void Texte::update(float time)
